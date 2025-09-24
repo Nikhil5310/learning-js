a75d6a2e809b19a7de1e318f60b3b78ce10721c5
Nikil5310 <nikhildhakad150@gmail.com>
2025-09-24 16:30:10 +0530

    first commit

---
+++ if else 22sep.js
@@ -0,0 +1,184 @@
+// if else famaliy
+// if , if then else , ladder if else , nested if else 
+
+
+// if (condition)
+// {
+
+// }
+// else
+//     {
+
+//     }
+
+
+// write a program to check number nagative or positive
+
+// let num =10
+// if (num > 0) {
+//     console.log("number is positive");
+// }
+// else{
+//     console.log("number is negative");
+// }
+
+// write a program  take two integer number and finde the grater number
+
+// let num1 = parseInt(prompt("enter first number"));
+// let num2 = parseInt(prompt("enter second number"));
+// if (num1 > num2) {
+//     console.log("num1 is grater");
+// }
+// else {
+//     console.log("num2 is grater");
+// }
+
+
+// write a program to check number even or odd 
+
+
+// let num = parseInt(prompt("enter a number"));   
+
+// if (num % 2 == 0) {
+//     console.log("number is even");
+// }
+// else {
+//     console.log("number is odd");
+// }
+
+
+// write a Program to take three number to user and to check second number even or odd and print multiplecation to all number and ans is odd to print addition
+
+// let num1 = parseInt(promt("enter frist numbr"))
+// let num2 = parseInt(promt("enter second numbr"))
+// let num3 = parseInt(promt("enter third numbr")) 
+// if (num2 % 2 == 0) {
+//     console.log(num1 * num2 * num3);
+// }
+// else {
+//     console.log(num1 + num2 + num3);
+
+// }
+
+//  ladder if else
+// if (condition1) {
+
+// }
+// else if (condition2) {
+// }    
+
+// else if (condition3) {
+
+// }
+// else {
+// }    
+
+// write a program to nuber posetive negative or newtral
+
+
+// let num = parseInt(prompt("enter a number"));
+// if (num > 0) {
+//     console.log("number is positive");
+// }
+// else if (num < 0){
+//     console.log("number is negative");
+// }
+// else{
+//     console.log("number is newtral");
+// }
+
+
+
+// write a program to take two input from the user and check which number is grater and also found out is eqal
+
+// let num1 = parseInt(prompt("enter first number"));
+// let num2 = parseInt(prompt("enter second number"));
+// if (num1 > num2) {
+//     console.log("num1 is grater ")
+// }
+// else if (num2 > num1) {
+//     console.log("num2 is grater")
+// }
+// else{
+//     console.log("both number are equal");
+// }
+
+
+// write a program to finde oute the percentage and grade
+
+// let marks = parseInt(prompt("enter your marks"));
+// let totalMarks = 500;
+// let percentage = (marks / totalMarks) * 100;
+// console.log("your percentage is " + percentage);
+// if (percentage >= 90) {
+//     console.log("your grade is A+");
+// }                 
+// else if (percentage >= 50) {
+//     console.log("your grade is b");
+// }
+// else if (percentage >= 40) {
+//     console.log("your grade is c");
+// }
+// else{
+
+//     console.log("you are fail");
+// }
+
+// nested if else
+
+// if (condition1) {
+
+//     if (condition2) {        
+//     }
+//     else {        
+//     }    
+// }
+// else {    
+//     if (condition3) {
+//     }
+// else {
+
+//     }
+// }
+
+
+// write a program for a indian citizen to vote
+
+// let age = parseInt(prompt("enter your age"));
+
+// if (age >= 18) {
+//     let b = parseInt(prompt("press 1 for indian"));
+//     if (b == 1) {
+//         console.log("you are eligible for vote");
+//     }   
+//     else {
+//         console.log("you are not eligible for vote");
+//     }   
+// }
+// else {
+//     console.log("you are not eligible for vote");
+// }
+
+
+
+
+
+
+// write a program to posative number even or odd than even number to srure and odd nuber to area of circle and  number is nagative to eqal -3 or not or print it
+
+// let num = parseInt(prompt("enter a number"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ loop23.js
@@ -0,0 +1,104 @@
+//  loop in js
+
+//  do while 
+//  while 
+//  for
+//  for in 
+//  for of
+//  for each
+
+
+// do {
+
+
+// }
+// while (condition);
+
+// then condition is true
+
+
+// while (condition) {
+
+// }
+
+
+// let a = 1
+// do {
+//     console.log('tv');
+//     a++
+// }while(a<10)
+
+
+
+// write a program to print 1 to 15
+
+// let a = 1
+// do {
+//     console.log(a);
+//     a++
+// } while (a <= 15)
+
+
+// write a program to print 10 to 1
+
+// let a = 10
+// do {
+//     console.log(a);
+//     a--
+// } while (a >= 1)    
+
+
+// write a program to all number 1 to Infinity but enter 0 to stop the loop
+
+// let a = 1
+// do {
+//     console.log(a);     
+//     a++
+//     let  num = parseInt(prompt("enter a number"))
+// } while (num != 0); 
+
+
+// const --------local
+// let  --------- local
+// var  global scope
+
+// write a program to print table in any number
+
+//  let num = parseInt(prompt("enter a number"));
+//     let a = 1   
+//     while (a <= 10) {
+//          console.log(num * a);
+//          a++
+//     }
+
+// write a program to print sum of all numbers between 15 to 1 
+
+// let num = parseInt(prompt("enter a number"));
+// let num1 = 0 
+// let a = 15
+// while (a >= 1) {
+//     num1 = num1 + a
+//     a--
+// }
+// console.log(num1);
+
+
+
+// for loop for(initialization; condition;is true then fist exicute the bloge {} and  increment/decrement)
+// {
+
+// }
+
+// write a program to print sume of all odd numbers between 1 to 15
+
+// let sum = 0
+// for (let a = 1; a <= 15; a++) {
+//      if (a % 2 != 0) { 
+//             sum = sum + a
+//             console.log(sum);
+//         }
+// }
+// // console.log(sum);
+
+
+
